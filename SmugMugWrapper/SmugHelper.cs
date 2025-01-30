64ce4a87558147627b16403c6b615597d909e230
Steve <steve@steveandkureen.com>
2025-01-30 10:51:55 -0700

    Update SmugHelper.cs

--- SmugMugWrapper/SmugHelper.cs
+++ SmugMugWrapper/SmugHelper.cs
@@ -4,8 +4,8 @@ namespace SmugMugWrapper
 {
    public static class SmugHelper
    {
-      public static string ApiKey { get { return "tHPWbv5ZsUY7kWqiBvXA8xG17oLXwE4m"; } }
-      public static string SecretApiKey { get { return "b23855a6bf9e8630ba1ba142bc8e7095"; } }
+      public static string ApiKey { get { return "{apiKye}"; } }
+      public static string SecretApiKey { get { return "{apiSecret}"; } }
 
       public static string ApiBaseUrl { get { return "https://api.smugmug.com"; } }
       public static string V1ApiSecureBaseUrl { get { return "https://secure.smugmug.com/services/api/json/1.3.0/"; } }
